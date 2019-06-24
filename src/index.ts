af437887a1c04b4d0b8f3aca1d909ca294126720
neripark <neripark@gmail.com>
2019-06-24 18:57:20 +0900

    add more test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 const testNum: number = 123;
 const testStr: string = 'this is test.';
+const testArr: string[] = ['hoge', 'fuga', 'piyo'];
 
 console.log(`${testNum} ${testStr}`);
 console.log('hogehoge!');
+console.log('----');
+
+console.log(...testArr);
