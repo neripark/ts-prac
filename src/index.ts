230aa6d66e7a1856397d4fecdbb9adab2fc65c63
neripark <neripark@gmail.com>
2019-10-29 17:55:30 +0900

    in progress

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,16 @@ const testNum: number = 123;
 const testStr: string = 'this is test.';
 const testArr: string[] = ['hoge', 'fuga', 'piyo'];
 
+
+Number.isSafeInteger(testNum);
+
+const arrayMapped = testArr.map(element => {
+  return element + "aaaaa! ";
+});
+
 console.log(`${testNum} ${testStr}`);
 console.log('hogehoge!');
 console.log('----');
 
 console.log(...testArr);
+console.log(...arrayMapped);
